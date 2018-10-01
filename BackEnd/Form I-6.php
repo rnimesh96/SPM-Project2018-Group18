9d8d2b0fe1cf4672391afea84ea3deb67fbed3c2
rnimesh <ravindunimesh1996@gmail.com>
2018-10-01 21:50:47 +0530

    #486,#479 create form i-6 and feed back form backend

--- BackEnd/Form I-6.php
+++ BackEnd/Form I-6.php
@@ -5,8 +5,7 @@
     <body>
     </body>
     <?php
-    $connection = mysql_connect("localhost", "username", "pw");  // Establishing Connection with Server
-    $db = mysql_select_db("dbname", $connection); // Selecting Database from Server
+   $mysqli = new mysqli("localhost", "root", "It12345@#1", "CSSE");
     if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
     $StudentName= $_POST['name'];
     $StudentID=$_POST['ITNO'];
@@ -20,15 +19,13 @@
     $LearnOut2= $_POST['LearnOut2'];
     $LearnOut3= $_POST['LearnOut3'];
     $CV1= $_POST['cv1'];
-    $CV1= $_POST['cv2'];
+    $CV2= $_POST['cv2'];
 
     if($Advertiser_name !=''){
         //Insert Query of SQL
-        $query = mysql_query("INSERT INTO form6(semester,year,date,StudentID,StudentName,Address,HomePhone,MobilePhone,Email,semester,year,CGPA,EmployeeName,
-        $StudentName,$StudentID,$Introduction1,$Introduction2,$Introduction3,$IntrenIns1,$IntrenIns2,$IntrenIns3,
-        $LearnOut1,$LearnOut2,$LearnOut3,$CV1,$CV2) values ('$semester','$year','$date','$StudentID','$StudentName','$Address','$HomePhone','$MobilePhone',
-    	'$Email','$semester','$year','$CGPA','$EmployeeName','$EmployeeAddress','$SupervisorName','$SupervisorPhone','$SupervisorTitle','$SupervisorEmail',
-    	'$InternshipStartDate','$InternshipEndDate','$NoOFHours','$studentTasks','$studentLearn','$ExternalSupervisorName','$Date'
+        $query = mysql_query("INSERT INTO Form_6(StudentID,year,date,StudentName,Introduction1,CompnyOverview,Introduction2,Introduction3,IntrenIns1,
+		IntrenIns2,IntrenIns3,LearnOut1,LearnOut2,LearnOut3,CV1,CV2) values ('$StudentID','$year','$date','$StudentName','$Introduction1','$Introduction2','$Introduction3',
+		'$IntrenIns1','$IntrenIns2','$IntrenIns3','$LearnOut1','$LearnOut2','$LearnOut3','$CV1','$CV2'
         )");
 
         echo "<br/><br/><span>Data Inserted successfully...!!</span>";
